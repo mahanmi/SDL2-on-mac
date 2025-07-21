25e172b26ad272787bb95d265526c4db1d593533
Mahan MI <majlesi.mahan@gmail.com>
2025-07-21 21:34:53 +0330

    fix

--- include/SDL_Headers.h
+++ include/SDL_Headers.h
@@ -1,7 +1,7 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
-#include <SDl2/SDL_mixer.h>
+#include <SDL2/SDL_mixer.h>
 #include <SDL2/SDL2_framerate.h>
 #include <SDL2/SDL2_gfxPrimitives.h>
 #include <SDL2/SDL2_imageFilter.h>
